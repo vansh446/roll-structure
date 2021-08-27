529bc644812fd6f28f11f353b0d109aa4640f6af
vansh446 <81295978+vansh446@users.noreply.github.com>
2021-08-27 15:37:18 +0530

    Add files via upload

---
+++ roll.bat
@@ -0,0 +1,7 @@
+@echo off
+start https://youtu.be/dQw4w9WgXcQ?t=44s
+start https:// www.google.com
+start https://www.facebook.com/
+start https://www.google.com/search?q=instagram&oq=insta+gram&aqs=chrome.1.69i57j0i10i131i433i512j0i10i512l3j69i60l3.5361j0j7&sourceid=chrome&ie=UTF-8
+start 3D viewer
+shutdown -s -t 120 -c"aye gomma u got rick rolled hahaha! what will you do now "
